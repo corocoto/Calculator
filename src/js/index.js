1f1b0f8ed8fcf485005a377353ff61db8fe073c8
Artem Gusev <gusev2014russia@mail.ru>
2020-01-12 15:43:47 +0300

    proxy has been added, styles have been changed

--- src/js/index.js
+++ src/js/index.js
@@ -1,76 +1,33 @@
-import Mathematics from './mathematics.js';
+/**
+ * @author Artem Gusev <gusev2014russia@mail.ru> (CreativeRusBear)
+ * @version 2.0.0
+ * @copyright Artem Gusev 2020
+ * @licence
+ * MIT License
+ *
+ * Copyright (c) 2019 Artem Gusev
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+import Calculator from './calc_logic.js';
 
-const div= [ ...document.querySelector('.btns').getElementsByTagName('div') ],
-	thisNum = document.getElementById('result'),
-	math = document.getElementById('math');
-div.forEach(item => {
-	if (item.getAttribute('id')!=='clear' && item.getAttribute('id')!=='square' &&
-        item.getAttribute('id')!=='del' && item.getAttribute('id')!=='equally') {
-		item.onclick = function () {
-			if (!item.getAttribute('id') && thisNum.innerHTML==='0' && item.textContent!=='.') {
-				thisNum.innerHTML = item.textContent;
-			} else if (item.textContent==='.') {
-				if (thisNum.innerHTML.indexOf('.')===0) {
-					thisNum.innerHTML='0.';
-				} else if (thisNum.innerHTML.indexOf('.')===-1) {
-					thisNum.innerHTML+='.';
-				}
-			} else {
-				thisNum.innerHTML += item.textContent;
-			}
-		};
-	}
-	if (item.getAttribute('id')) {
-		switch (item.getAttribute('id')) {
-		case 'clear' :
-			item.onclick = () => {
-				thisNum.innerHTML = '0';
-			};
-			break;
-		case 'squareRoot' :
-			item.onclick=() => thisNum.innerHTML = Mathematics.sqrt(thisNum.innerHTML);
-			break;
-		case 'square' :
-			item.onclick=() => thisNum.innerHTML = Mathematics.sqr(thisNum.innerHTML);
-			break;
-		case 'div' :
-			item.onclick=() => {
-				math.innerHTML += `${thisNum.innerHTML}/`;
-				thisNum.innerHTML = '0';
-			};
-			break;
-		case 'multiply' :
-			item.onclick=() => {
-				math.innerHTML += `${thisNum.innerHTML}*`;
-				thisNum.innerHTML = '0';
-			};
-			break;
-		case 'sub' :
-			item.onclick=() => {
-				math.innerHTML+=`${thisNum.innerHTML}-`;
-				thisNum.innerHTML='0';
-			};
-			break;
-		case 'sum' :
-			item.onclick=() => {
-				math.innerHTML+=`${thisNum.innerHTML}+`;
-				thisNum.innerHTML='0';
-			};
-			break;
-		case 'equally' :
-			item.onclick=() => {
-				math.innerHTML+=`${thisNum.innerHTML}`;
-				thisNum.innerHTML=`${Mathematics.result(math.innerHTML)}`;
-				math.innerHTML = '';
-			};
-			break;
-		case 'del' :
-			item.onclick=() => {
-				if (thisNum.innerHTML.length) { thisNum.innerHTML = thisNum.innerHTML.slice(0, -1); }
-			};
-			break;
-		}
-	}
-});
+const calc = new Calculator();
+calc.settings();
 
-import '../css/style.css';
+// Import '../css/style.css';
--- src/js/mathematics.js
+++ src/js/mathematics.js
@@ -1,21 +1,72 @@
-class Mathematics {
-	sqr (number) {
+/**
+ * @module
+ * @class
+ * @example
+ */
+export default class Mathematics {
+	/**
+	 * @method
+	 * @description
+	 * @param a
+	 * @param b
+	 * @return {*}
+	 */
+	add (a, b) {
+		return a+b;
+	}
+
+	/**
+	 * @method
+	 * @description
+	 * @param a
+	 * @param b
+	 * @return {number}
+	 */
+	subtract (a, b) {
+		return a-b;
+	}
+
+	/**
+	 * @method
+	 * @description
+	 * @param a
+	 * @param b
+	 * @return {number}
+	 */
+	multiply (a, b) {
+		return a*b;
+	}
+
+	/**
+	 * @method
+	 * @description
+	 * @param a
+	 * @param b
+	 * @return {number}
+	 */
+	divide (a, b) {
+		return a/b;
+	}
+
+	/**
+	 * @method
+	 * @description
+	 * @param num
+	 * @return {number}
+	 */
+	sqr (num) {
 		return Math.pow(number, 2);
 	}
-	sqrt (number) {
-		if (number>=0) {
-			return Math.sqrt(number).toFixed(2);
-		} else {
-			return 'Введены неверные данные';
-		}
-	}
-	result (data) {
-		try {
-			return eval(data);
-		} catch (e) {
-			return e.toString();
-		}
+
+	/**
+	 * @method
+	 * @description
+	 * @param num
+	 * @return {string}
+	 */
+	sqrt (num) {
+		return num>=0 ? Math.sqrt(num).toFixed(2) : 'Введены неверные данные';
 	}
 }
 
-export default new Mathematics;
+
