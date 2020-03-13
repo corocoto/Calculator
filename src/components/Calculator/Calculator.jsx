a9a6af23701dae2fcdd872be9bfcf5f064e0a0db
Artem Gusev <gusev2014russia@mail.ru>
2020-03-14 01:41:34 +0300

    adding main math operations

--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -4,78 +4,137 @@ import styles from './Calculator.module.css';
 import Display from '../Display/Display';
 import Button from '../Button/Button';
 
-export default class Calculator extends Component{
+export default class Calculator extends Component {
+    /**
+     * @constructor
+     * @param {object} props - props object
+     */
     constructor(props) {
         super(props);
-        this.state = {
-            result: 0,
-            current: 0,
-            operation: null,
+        this.state = this.initialState();
+    }
+
+    componentDidMount() {
+        // this.addEventListener();
+        this.calcOperations = {
+            '/': (prevValue, currentValue) => prevValue / currentValue,
+            '*': (prevValue, currentValue) => prevValue * currentValue,
+            '+': (prevValue, currentValue) => Number(prevValue) + Number(currentValue),
+            '-': (prevValue, currentValue) => prevValue - currentValue,
+            '=': currentValue => currentValue,
+            '√': currentValue => Math.sqrt(currentValue),
+            'x²': currentValue => currentValue**2
         };
     }
 
-    clickEventHandler(symbol){
-        if (this.state.operation && Number.isNaN(symbol) && !this.state.current){
-            this.changeOperation(symbol);
-        }else if (Number.isNaN(symbol)){
+    shouldComponentUpdate(nextProps, nextState, nextContext) {
+        // if (nextState.currentVal !== this.state.currentVal) {
+            return true;
+        // }
+        // if (nextState.operation) {
+        //
+        // }
+        // return false;
+    }
 
+
+    componentWillUnmount() {
+        // this.removeEventListener();
+    }
+
+
+    /**
+     * @method
+     * @description
+     * @returns {Object} - initial state
+     */
+    initialState() {
+        return {
+            operation: null,
+            currentVal: 0,
+            result: 0,
+            math: null
+        }
+    }
+
+    /**
+     * @method
+     * @description
+     * @param {number | string} symbol
+     */
+    writeNum(symbol){
+        const value = (symbol === '.' || this.state.currentVal)
+            ? this.state.currentVal + symbol
+            : symbol;
+        this.setState({currentVal: value});
+    }
+
+    /**
+     * @method
+     * @description
+     */
+    setPrevNumState(){
+        this.setState({currentVal: this.state.currentVal.slice(0, -1)});
+    }
+
+    /**
+     * @method
+     * @param {string} symbol
+     */
+    choiceOperation(symbol){
+        let {operation, currentVal, result} = this.state;
+        if (['√','x²'].includes(symbol)){
+            currentVal = this.calcOperations[symbol](currentVal);
+            this.setState({currentVal});
+            return;
+        }
+        if (operation){
+            currentVal = this.calcOperations[operation](result, currentVal);
+        }
+        this.setState({
+            currentVal: symbol=== '=' ? currentVal : 0,
+            result: currentVal,
+            operation: symbol
+        });
+    }
+
+    /**
+     * @method
+     * @description
+     * @param {string | number} symbol
+     */
+    clickEventHandler(symbol) {
+        if (/(\d|\.)/.test(symbol)) {
+            this.writeNum(symbol);
+        }else if (symbol === 'Del') {
+            this.setPrevNumState();
+        }else if (symbol === 'C') {
+            this.setState(this.initialState());
         }else{
-            const value = this.state.current ? this.state.current + String(symbol) : symbol>0 && symbol;
-            this.setState({
-                current: value
-            })
+            this.choiceOperation(symbol);
         }
     }
-
-    changeOperation(operation){
-        switch (operation) {
-            case 'C':
-                this.setState({
-                    result: 0,
-                    current: 0,
-                    operation: null
-                });
-                break;
-            case '=':
-                this.setState({
-                    current: 0,
-                    operation: null
-                });
-                break;
-            case 'Del':
-                return;
-                break;
-            default:
-                this.setState({
-                    operation
-                });
-        }
-    }
-
 
     render() {
         const btnsVal = [
             'C', '√', 'x²', '/',
-             7, 8, 9, '*',
-             4, 5, 6, '-',
-             1, 2, 3, '+',
-            '.', 0, 'Del','='
+            '7', '8', '9', '*',
+            '4', '5', '6', '-',
+            '1', '2', '3', '+',
+            '.', '0', 'Del', '='
         ];
-        const btnsArr = btnsVal.map((val,index)=>{
+        const btnsArr = btnsVal.map((val, index) => {
             return <Button
                 key={`${index}-${val}`}
                 value={val}
-                onClick={()=>this.clickEventHandler(val)}
+                onClick={() => this.clickEventHandler(val)}
             />
         });
         return (
             <div className={styles.Calculator}>
                 <Display
-                    math={this.state.operation
-                        ? `${this.state.result} ${this.state.operation} ${this.state.current}`
-                        : this.state.result > 0 && this.state.result
-                    }
-                    result={this.state.result}
+                    math={this.math}
+                    result={this.state.currentVal}
                 />
                 <div className={styles.btnsBlock}>
                     {btnsArr}
