18ef986b1952f2935e06273581cf1110be7218ad
Artem Gusev <gusev2014russia@mail.ru>
2020-10-09 09:08:03 +0300

    adds actionTypes file

---
+++ src/store/actions/actionTypes.js
@@ -0,0 +1,25 @@
+const ADD = 'ADD';
+const SUBTRACT = 'SUBTRACT';
+const DIVIDE = 'DIVIDE';
+const MULTIPLY = 'MULTIPLY';
+const EXPONENTIATION ='EXPONENTIATION'; //pow
+const SQUARE_ROOT = 'SQUARE_ROOT';
+const GET_RESULT = 'GET_RESULT';
+
+const CLEAR_ALL = 'CLEAR_ALL';
+const BACKSPACE = 'BACKSPACE';
+
+module.exports = {
+  /** Math operations */
+  ADD,
+  SUBTRACT,
+  DIVIDE,
+  MULTIPLY,
+  EXPONENTIATION,
+  SQUARE_ROOT,
+  GET_RESULT,
+
+  /** Other operations */
+  CLEAR_ALL,
+  BACKSPACE
+};
