8c07b5f52adcf3a3a360f159ffebc08bad1a312b
Artem Gusev <gusev2014russia@mail.ru>
2020-10-12 19:17:43 +0300

    adds interlayer between action creators and it dispatch

---
+++ src/store/actions/index.js
@@ -0,0 +1,13 @@
+export {
+    add,
+    subtract,
+    multiply,
+    divide,
+    exponentiation
+} from './math';
+
+export {
+    clearAll,
+    backspace,
+    getResult
+} from './otherOperations';
